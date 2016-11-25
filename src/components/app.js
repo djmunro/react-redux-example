4ffb2779c03a8f279fcb9c3a64cbd03bdd8439d3
David Munro <Dave.Munro@harman.com>
2016-11-25 11:19:07 -0500

    Initial use of redux in sample app

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserList from '../containers/user-list';
+
+
+const App = () => (
+    <div>
+        <h2>User List</h2>
+        <UserList />
+        <hr />
+        <h2>User Details</h2>
+    </div>
+);
+
+export default App;
---
+++ src/reducers/reducer-users.js
@@ -0,0 +1,28 @@
+export default function () {
+  return [
+    {
+      id: 1,
+      first: "Dave",
+      last: "Munro",
+      age: 30,
+      description: "Dave loves big fluffy cats",
+      thumbnail: "http://i.imgur.com/4pe1eeQ.jpg"
+    },
+    {
+      id: 2,
+      first: "Ryan",
+      last: "Munro",
+      age: 28,
+      description: "Ryan likes ponnies",
+      thumbnail: "http://i.imgur.com/DQocqZZ.mp4"
+    },
+    {
+      id: 3,
+      first: "Kim",
+      last: "Munro",
+      age: 52,
+      description: "i LIKE COOKIES",
+      thumbnail: "http://i.imgur.com/wd5uuwT.jpg"
+    }
+  ]
+}
